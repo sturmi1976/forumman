829bc0489af02a7e211d4f61aae2adb4fe88d7da
Andre Lanius <info@andre-lanius.de>
2026-06-05 18:41:03 +0200

    Release v1.8.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'a-lanius@web.de',
     'author_company' => 'Andre Lanius',
 
-    'version' => '1.7.1',
+    'version' => '1.8.0',
 
     'constraints' => [
         'depends' => [
